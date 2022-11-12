c2427017caa2df498cb25ad30ee1bc268618bf1e
jbotello <jbotello@uei.com>
2022-11-12 14:52:22 -0300

    Add Counter class

--- inc/module01.hpp
+++ inc/module01.hpp
@@ -1,4 +1,18 @@
 #ifndef __MAIN_CPP__
 #define __MAIN_CPP__
 
+/**
+ * @brief Class that keeps a count
+ */
+
+class Counter
+{
+public: 
+  Counter(int rst_val, int inc_val);
+  void increment(int delta);
+  void decrement(int delta);
+  void reset(int val);
+};
+
+
 #endif /* __MAIN_CPP__ */
